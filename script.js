4bd13ddd20e9c84ae85bd3fcae623eac8a129ea8
Cara Noyes <cnnoyes@outlook.com>
2021-06-09 13:58:22 -0500

    completed studio

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// TODO: add code here
+
+
+window.addEventListener('load', function() {
+    let json = [];
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            console.log('json',json);
+            console.log('length', Object.keys(json).length);
+            const container = document.getElementById("container");
+            for (let i = 0; i <= Object.keys(json).length; i++) {
+            container.innerHTML += 
+            `<div class="astronaut">
+            <div class="bio">
+               <h3>${json[i].firstName} ${json[i].lastName}</h3>
+               <ul>
+                  <li>Hours in space: ${json[i].hoursInSpace}</li>
+                  <li>Active: ${json[i].active}</li>
+                  <li>Skills: ${json[i].skills}</li>
+               </ul>
+            </div>
+            <img class="avatar" src="${json[i].picture}">
+         </div>
+         `;
+            }   // container.innerHTML = `<h3> Astronaut ${json[i].firstName} </h3>`;
+        }); 
+    });
+});
